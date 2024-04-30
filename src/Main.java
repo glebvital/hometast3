79f037076ed086a18f7958639534b9d6770ffbd9
Your Name <git config --global user.email>
2024-04-30 17:26:52 +0000

    lil changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main{
     public static void main(String[] args) {
-
+        System.out.println("helo");
     }
 }
 //git init == create project
 //git add . == add file for git that he's following
 //git commit -m "add login & password" == saving project
 //git remote add origin https://github.com/glebvital/hometast3.git == add project in interned
+//https://github.com/glebvital/hometast3/blob/master/src/Main.java
