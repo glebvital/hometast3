c768682a9c63d477b2dcc3b5395dffa09b1c6ec5
Your Name <git config --global user.email>
2024-04-30 17:56:05 +0000

    lil song and code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println("helo, i am realy glad that you're looking that. So i decided to sing beautiful song. Wachamelaindisama..... Wanabrokdisaout.....Siouot.....Whenamisterbrown");
+        System.out.println("---");
+        MassiveProblem(1);
+        MassiveProblem(2);
+        MassiveProblem(3);
+        MassiveProblem(4);
+        MassiveProblem(5);
+        MassiveProblem(6);
+        //MassiveProblem(7); - very bad line of code
+    }
+    public static void MassiveProblem(int day){
+        String[] week = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        System.out.println(week[day]);
+        System.out.println("---");
     }
 }
 //git init == create project
