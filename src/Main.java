9d31fad4697413ede8be62d883ab5214a08f8596
Your Name <git config --global user.email>
2024-04-30 17:14:14 +0000

    add login and password

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+
+    }
+}
+//git init == create project
+//git add . == add file for git that he's following
+//git commit -m "add login & password" == saving project
+//git remote add origin https://github.com/glebvital/hometast3.git == add project in interned
