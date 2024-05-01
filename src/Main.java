8d186922fcc240eb6fd09ef2b429f1496ffafab8
gleb <git config --global user.email>
2024-05-01 14:21:48 +0000

    ✓

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ public class Main{
     public static void main(String[] args) {
         System.out.println("helo, i am realy glad that you're looking that. So i decided to sing beautiful song. Wachamelaindisama..... Wanabrokdisaout.....Siouot.....Whenamisterbrown");
         System.out.println("---");
+        MassiveProblem(0);
         MassiveProblem(1);
         MassiveProblem(2);
         MassiveProblem(3);
         MassiveProblem(4);
         MassiveProblem(5);
         MassiveProblem(6);
-        //MassiveProblem(7); - very bad line of code
+        ExtraMassive(6,7,7,3);
     }
     public static void MassiveProblem(int day){
         String[] week = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
         System.out.println(week[day]);
         System.out.println("---");
+    }
+    public static void ExtraMassive(int num1,int num2,int num3,int num4){
+        String[] something = {"a","b","c","d","e","f","g","o"};
+        System.out.println(something[num1]+something[num2]+something[num3]+something[num4]);
     }
 }
 //git init == create project
